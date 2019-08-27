8a74537aad4d50cf4204b9a04431bfd2d3bd9ff2
佐藤 禎哉 <satotomoya@LAB-N1648.local>
2019-08-27 10:42:07 +0900

    コントローラ、サービスの修正

--- src/main/java/com/example/demo/controller/TodoController.java
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -8,6 +8,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+import java.util.Optional;
+
 import javax.servlet.http.HttpServletRequest;
 import com.example.demo.entity.MyData;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,33 +27,79 @@ public class TodoController {
 
     @RequestMapping(value = "/", method=RequestMethod.GET)
     public ModelAndView index(@ModelAttribute("formModel") MyData mydata, ModelAndView mav) {
-        return todoservice.index(mydata, mav);
+        mav.setViewName("index");
+        mav.addObject("msg", "this is sample content.");
+        List<MyData> list = todoservice.searchbyisdone(mydata);
+        mav.addObject("data", list);
+        return mav;
     }
 
     @RequestMapping(value="/", method=RequestMethod.POST)
     @Transactional(readOnly=false)
     public ModelAndView form(@ModelAttribute("formModel") @Validated MyData mydata, BindingResult result, ModelAndView mav) {
-        return todoservice.form(mydata, result, mav);
+        ModelAndView res = null;
+        if (!result.hasErrors()) {
+            todoservice.save(mydata);
+            res = new ModelAndView("redirect:/");
+        } else {
+            mav.setViewName("index");
+            mav.addObject("msg", "sorry, error is occured...");
+            List<MyData> list = todoservice.searchbyisdone(mydata);
+            mav.addObject("data", list);
+            res = mav;
+        }
+        return res;
     }
 
     @RequestMapping(value="/edit/{id}", method=RequestMethod.GET)
     public ModelAndView edit(@ModelAttribute MyData mydata, @PathVariable int id, ModelAndView mav) {
-        return todoservice.edit(mydata, id, mav);
+        mav.setViewName("edit");
+        mav.addObject("title", "edit mydata.");
+        Optional<MyData> data = todoservice.searchbyid(mydata, id);
+        mav.addObject("formModel", data.get());
+        return mav;
     }
 
     @RequestMapping(value="/edit/{id}", method=RequestMethod.POST)
     @Transactional(readOnly=false)
     public ModelAndView update(@ModelAttribute MyData mydata, @RequestParam(value="checkisdone", required=false)boolean checkisdone, ModelAndView mav) {
-        return todoservice.update(mydata, checkisdone, mav);
+        todoservice.update(mydata, checkisdone);
+        return new ModelAndView("redirect:/");
     }
 
     @RequestMapping(value="/search", method=RequestMethod.GET)
     public ModelAndView find(@ModelAttribute MyData mydata, ModelAndView mav, HttpServletRequest request) {
-        return todoservice.find(mydata, mav, request);
+        mav.setViewName("find");
+        mav.addObject("title", "Find page");
+        mav.addObject("msg", "MyDataのサンプルです。");
+        mav.addObject("value", "");
+        return mav;
     }
 
     @RequestMapping(value="/search", method=RequestMethod.POST)
-    public ModelAndView search(HttpServletRequest request, ModelAndView mav) {
-        return todoservice.search(request, mav);
+    public ModelAndView search(HttpServletRequest request, @ModelAttribute MyData mydata, ModelAndView mav) {
+        mav.setViewName("find");
+        String content = request.getParameter("content");
+        content = "%"+content+"%";
+        String category = request.getParameter("category");
+        if ("%%".equals(content)) {
+            mav.addObject("title", "Find result");
+            mav.addObject("msg", "検索結果");
+            mav.addObject("content", "");
+            mav.addObject("category", "");
+            List<MyData> data = todoservice.searchbycategory(mydata, category);
+            mav.addObject("formModel", data);
+            
+        }
+        else {
+            mav.addObject("title", "Find result");
+            mav.addObject("msg", "検索結果");
+            mav.addObject("content", "");
+            mav.addObject("category", "");
+            List<MyData> data = todoservice.searchbycontentorcategory(mydata, content, category);
+            mav.addObject("formModel", data);
+            
+        }
+        return mav;
     }
 }
--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -17,78 +17,36 @@ public class TodoService {
     @Autowired
     MyDataRepository repository;
 
-    public ModelAndView index(MyData mydata, ModelAndView mav) {
-        mav.setViewName("index");
-        mav.addObject("msg", "this is sample content.");
+    public List<MyData> searchbyisdone(MyData mydata) {
         List<MyData> list=repository.findByIsdone(false);
-        mav.addObject("data", list);
-        return mav;
+        return list;
     }
 
-    public ModelAndView form(MyData mydata, BindingResult result, ModelAndView mav) {
-        ModelAndView res = null;
-        if (!result.hasErrors()) {
-            repository.saveAndFlush(mydata);
-            res = new ModelAndView("redirect:/");
-        } else {
-            mav.setViewName("index");
-            mav.addObject("msg", "sorry, error is occured...");
-            List<MyData> list=repository.findByIsdone(false);
-            mav.addObject("data", list);
-            res = mav;
-        }
-        return res;
+    public void save(MyData mydata) {
+        repository.saveAndFlush(mydata);
     }
 
-    public ModelAndView edit(MyData mydata, int id, ModelAndView mav) {
-        mav.setViewName("edit");
-        mav.addObject("title", "edit mydata.");
-        Optional<MyData> data = repository.findById(id);
-        mav.addObject("formModel", data.get());
-        return mav;
+    public Optional<MyData> searchbyid(MyData mydata, int id) {
+        return repository.findById(id);
     }
 
-    public ModelAndView update(MyData mydata, boolean checkisdone, ModelAndView mav) {
-        
+    public void update(MyData mydata, boolean checkisdone) {
         if (checkisdone) {
             mydata.setIsdone(true);
         }
         repository.saveAndFlush(mydata);
-        return new ModelAndView("redirect:/");
     }
 
-    public ModelAndView find(MyData mydata, ModelAndView mav, HttpServletRequest request) {
-        mav.setViewName("find");
-        mav.addObject("title", "Find page");
-        mav.addObject("msg", "MyDataのサンプルです。");
-        mav.addObject("value", "");
-        return mav;
+    public List<MyData> searchbycategory(MyData mydata, String category) {
+        List<MyData> data = repository.findByCategory(category);
+            
+        return data;
     }
 
-    public ModelAndView search(HttpServletRequest request, ModelAndView mav) {
-        mav.setViewName("find");
-        String content = request.getParameter("content");
-        content = "%"+content+"%";
-        String category = request.getParameter("category");
-        if ("%%".equals(content)) {
-            mav.addObject("title", "Find result");
-            mav.addObject("msg", "検索結果");
-            mav.addObject("content", "");
-            mav.addObject("category", "");
-            List<MyData> data = repository.findByCategory(category);
-            mav.addObject("formModel", data);
-            
-        }
-        else {
-            mav.addObject("title", "Find result");
-            mav.addObject("msg", "検索結果");
-            mav.addObject("content", "");
-            mav.addObject("category", "");
-            List<MyData> data = repository.findByContentLikeOrCategory(content, category);
-            mav.addObject("formModel", data);
-            
-        }
-        return mav;
+    public List<MyData> searchbycontentorcategory(MyData mydata, String content, String category) {
+        List<MyData> data = repository.findByContentLikeOrCategory(content, category);
+
+        return data;
     }
 
 }
